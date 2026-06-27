894650e75d421e68353445ba1b1951660eb15a1e
Frogzalcoatl <saharholmes@gmail.com>
2026-06-27 00:44:45 -0400

    Add log formatting

---
+++ src/FormatLogs.cpp
@@ -0,0 +1,78 @@
+#include "FormatLogs.hpp"
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+static std::string getTimeStamp() {
+    SDL_Time ticks;
+    SDL_DateTime dt;
+    if (SDL_GetCurrentTime(&ticks) && SDL_TimeToDateTime(ticks, &dt, true)) {
+        char buf[16];
+        std::snprintf(buf, sizeof(buf), "%02d:%02d:%02d", dt.hour, dt.minute, dt.second);
+        return buf;
+    } else {
+        return "00:00:00";
+    }
+}
+
+static std::string priorityToString(SDL_LogPriority priority) {
+    switch (priority) {
+    case SDL_LOG_PRIORITY_INVALID:
+        return "INVALID";
+    case SDL_LOG_PRIORITY_TRACE:
+        return "TRACE";
+    case SDL_LOG_PRIORITY_VERBOSE:
+        return "VERBOSE";
+    case SDL_LOG_PRIORITY_DEBUG:
+        return "DEBUG";
+    case SDL_LOG_PRIORITY_INFO:
+        return "INFO";
+    case SDL_LOG_PRIORITY_WARN:
+        return "WARN";
+    case SDL_LOG_PRIORITY_ERROR:
+        return "ERROR";
+    case SDL_LOG_PRIORITY_CRITICAL:
+        return "CRITICAL";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+static std::string categoryToString(int category) {
+    switch (category) {
+    case SDL_LOG_CATEGORY_APPLICATION:
+        return "APPLICATION";
+    case SDL_LOG_CATEGORY_ERROR:
+        return "ERROR";
+    case SDL_LOG_CATEGORY_ASSERT:
+        return "ASSERT";
+    case SDL_LOG_CATEGORY_SYSTEM:
+        return "SYSTEM";
+    case SDL_LOG_CATEGORY_AUDIO:
+        return "AUDIO";
+    case SDL_LOG_CATEGORY_VIDEO:
+        return "VIDEO";
+    case SDL_LOG_CATEGORY_RENDER:
+        return "RENDER";
+    case SDL_LOG_CATEGORY_INPUT:
+        return "INPUT";
+    case SDL_LOG_CATEGORY_TEST:
+        return "TEST";
+    case SDL_LOG_CATEGORY_GPU:
+        return "GPU";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+static void sdlOutput(void* userdata, int category, SDL_LogPriority priority, const char* message) {
+    (void)userdata;
+    if (!message) {
+        return;
+    }
+    std::string timeStamp = getTimeStamp();
+    std::cout << "[" << timeStamp << "] [" << categoryToString(category) << "/"
+              << priorityToString(priority) << "]: " << message << std::endl;
+}
+
+void initLogFormatting() { SDL_SetLogOutputFunction(sdlOutput, nullptr); }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "FormatLogs.hpp"
 #include "Gamepad.hpp"
 #include "platformer.hpp"
 #include <imgui.h>
@@ -5,6 +6,7 @@
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
+    initLogFormatting();
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD | SDL_INIT_AUDIO)) {
         SDL_Log("SDL Initialization failed: %s", SDL_GetError());
         return 1;
