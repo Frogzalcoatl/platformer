30ca71f104f8788c95f7eaaa8433e14c661a2eb1
Frogzalcoatl <saharholmes@gmail.com>
2026-07-13 18:46:34 -0400

    Rename project to MyPlatformer

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -300,7 +300,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
      */
     protected String[] getLibraries() {
         return new String[] {
-                "MyAndroidProject"
+                "MyPlatformer"
         };
     }
 
